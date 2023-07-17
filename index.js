b2c61fd80621df81454bff04dbe4f0ca7762f9eb
connorq <connorquitt@gmail.com>
2023-07-17 18:03:19 -0500

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(thing) {
+    return [thing[0], thing[1]];
+}
+
+const returnLastTwoDrivers = function(thing) {
+    return [thing[2], thing[3]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function fareMultiplier(multiplier=fare) {
+        return fare * multiplier;
+    }
+}
+
+function fareDoubler(fare) {
+    return createFareMultiplier(fare)(2);
+    }
+
+function fareTripler(fare) {
+    return createFareMultiplier(fare)(3);
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
